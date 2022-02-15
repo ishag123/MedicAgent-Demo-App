1411ec4fd687828659bf09935205b250dc0c1fd8
Isha Gosalia <ishagosalia1@gmail.com>
2022-02-15 09:28:49 -0800

    Initial Commit

---
+++ MedicAgent Demo App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedicAgent Demo App
+//
+//  Created by Isha Gosalia on 2/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
